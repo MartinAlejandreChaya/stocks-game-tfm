eda9e08d0ab25c950fe9b070e42d5fcae6e62652
MartinAlejandreChaya <martin.alejandre.chaya@gmail.com>
2024-02-15 12:11:28 +0100

    Setup database management

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask
+import db_manager
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Hello world!"
+
+@app.route("/test_db")
+def test_db():
+
+    db_manager.add_row({"name": "", "move_id": 15,
+                        "state_day": 17, "state_price": 16.41, "state_other_selled": True,
+                        "action": 1, "result": -15})
+
+    return "Success"
