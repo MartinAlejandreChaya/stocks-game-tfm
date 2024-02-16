f55c543da4b56ee78efd4d6e1b978a637278c9ea
MartinAlejandreChaya <martin.alejandre.chaya@gmail.com>
2024-02-16 18:43:07 +0100

    Initial mock-up and some JS functionality

---
+++ static/js/general.js
@@ -0,0 +1,42 @@
+
+
+window.onload = () => {
+
+    // Rules hide button
+    const rules_hide_button = document.getElementById("rules-hide-button")
+    const rules_p = document.getElementById("rules-p")
+    let rules_button_hidden = false;
+    rules_p.style.maxHeight = '300px';
+    rules_hide_button.addEventListener('click', (ev) => {
+        if (rules_button_hidden) {
+            rules_p.style.maxHeight = '300px'
+            rules_hide_button.textContent = "Hide"
+        }
+        else {
+            rules_p.style.maxHeight = '0px'
+            rules_hide_button.textContent = "Show rules"
+        }
+        rules_button_hidden = !rules_button_hidden;
+    })
+
+    // Play button
+    const play_button = document.getElementById("play-button");
+    play_button.addEventListener('click', () => {
+        /* Send play to server */
+        /* Server will respond with OK or with 'Master user' */
+        /* Then do this
+            * Hide rules
+            * Fix user name
+            * Hide play button
+            * Display play component
+                - An indicator that visually and explicitly shows
+                the game state: (day, price, other_selled)
+                - Two buttons (sell, don't sell)
+            * Display username playing bar that fills up as you generate more data
+            * Show user average
+            * Show some strategies averages.
+            * Show current database average (?) Maybe this introduces bias
+
+        */
+    })
+}
